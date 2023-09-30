a53983bf43c5884b28c5015e66b76e681a778ec2
Riyso <96721138+Riyso@users.noreply.github.com>
2023-09-30 18:27:04 +0300

    Add files via upload

--- ghettoh.lua
+++ ghettoh.lua
@@ -16,7 +16,7 @@ u8 = encoding.UTF8
 update_state = false
 
 local script_vers = 0
-local script_vers_text = "1.05"
+local script_vers_text = "1.5"
 
 local update_url = "https://raw.githubusercontent.com/Riyso/version/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
